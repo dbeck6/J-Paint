b01025299142fc3d5ae07c8301489e1a23e71ce1
David Beck <davidernestbeck@gmail.com>
2018-07-22 20:22:36 -0500

    created Shape classes

---
+++ model/shapes/Rectangle.java
@@ -0,0 +1,10 @@
+package model.shapes;
+
+import model.interfaces.IDrawShapesStrategy;
+
+public class Rectangle implements IDrawShapesStrategy {
+    @Override
+    public int drawShapes(int x1, int y1, int x2, int y2) {
+        return 0;
+    }
+}
